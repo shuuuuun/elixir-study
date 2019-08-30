77567fe3ee6ebc5284445421a1c920d90b767b89
shun <shun@shuuuuun.com>
2019-08-30 13:47:33 +0000

    chop

--- chop.exs
+++ chop.exs
@@ -1,10 +1,26 @@
 defmodule Chop do
   def guess(actual, range) do
     min..max = range
-    middle = div(max - min, 2)
+    middle = div(max - min, 2) + min
     IO.puts "Is it #{middle}"
+    helper(actual, range, middle)
   end
 
-  defp helper(actual, range, guess) do
+  defp helper(actual, range, guess) when actual === guess do
+    IO.puts "#{guess}"
+  end
+
+  defp helper(actual, range, guess) when actual < guess do
+    min..max = range
+    middle = div(actual - min, 2) + min
+    IO.puts "Is it #{middle}"
+    helper(actual, range, middle)
+  end
+
+  defp helper(actual, range, guess) when actual > guess do
+    min..max = range
+    middle = div(max - actual, 2) + actual
+    IO.puts "Is it #{middle}"
+    helper(actual, range, middle)
   end
 end
