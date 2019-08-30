e84ff303ab7cc8e65471abc096b0a0b80f2cfe31
motoki-shun <motoki-shun@kayac.com>
2019-08-30 23:39:34 +0900

    refactor

--- chop.exs
+++ chop.exs
@@ -3,10 +3,6 @@
 
 defmodule Chop do
   def guess(actual, min..max) do
-    check(actual, min, max)
-  end
-
-  defp check(actual, min, max) do
     middle = div(max - min, 2) + min
     IO.puts "Is it #{middle}"
     helper(actual, min..max, middle)
@@ -18,11 +14,11 @@ defmodule Chop do
 
   defp helper(actual, min.._max, guess) when actual < guess do
     max = guess - 1
-    check(actual, min, max)
+    guess(actual, min..max)
   end
 
   defp helper(actual, _min..max, guess) when actual > guess do
     min = guess + 1
-    check(actual, min, max)
+    guess(actual, min..max)
   end
 end
