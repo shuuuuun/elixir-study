1ddd846e6c267fd4223c0358b9a44bc2b1263a4a
shuuuuuny <shuuuuuny+github@gmail.com>
2019-09-08 16:15:28 +0900

    MyEnum.all?

--- myenum.exs
+++ myenum.exs
@@ -1,5 +1,10 @@
 defmodule MyEnum do
+  # MyEnum.all? [], &(&1 < 4)
+  # MyEnum.all? [1, 2, 3], &(&1 < 4)
   # MyEnum.all? [1, 2, 3, 4, 5], &(&1 < 4)
+  def all?([], _), do: true
+  def all?([head | tail], func), do: func.(head) && all?(tail, func)
+
   # MyEnum.each [1, 2, 3, 4, 5], &(&1 * 10)
   # MyEnum.filter [1, 2, 3, 4, 5], &(&1 < 4)
   # MyEnum.split [1, 2, 3, 4, 5], 3
