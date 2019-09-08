0f01dce1e0e361d5f07bd129b8d8ad7da6f61a0c
shuuuuuny <shuuuuuny+github@gmail.com>
2019-09-08 15:54:03 +0900

    add myenum.exs

---
+++ myenum.exs
@@ -0,0 +1,7 @@
+defmodule MyEnum do
+  # MyEnum.all? [1, 2, 3, 4, 5], &(&1 < 4)
+  # MyEnum.each [1, 2, 3, 4, 5], &(&1 * 10)
+  # MyEnum.filter [1, 2, 3, 4, 5], &(&1 < 4)
+  # MyEnum.split [1, 2, 3, 4, 5], 3
+  # MyEnum.take [1, 2, 3, 4, 5]. 3
+end
