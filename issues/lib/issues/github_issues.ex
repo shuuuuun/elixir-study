d2bcfdb23a041c4fc8e4c0cce19318137dc56c09
motoki-shun <motoki-shun@kayac.com>
2019-09-16 12:02:16 +0900

    parseしてなかった

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -15,7 +15,7 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({ :ok, %HTTPoison.Response{ status_code: 200, body: body } }) do
-    { :ok, body }
+    { :ok, Poison.Parser.parse!(body) }
   end
 
   def handle_response({ _, %HTTPoison.Error{ reason: reason } }) do
