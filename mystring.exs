fd275156c7406bfd314e20c075fbbf8f9ae9b78a
shuuuuuny <shuuuuuny+github@gmail.com>
2019-09-16 10:09:48 +0900

    MyString.anagram?

--- mystring.exs
+++ mystring.exs
@@ -3,4 +3,11 @@ defmodule MyString do
   def printable?(str) when is_binary(str), do: true
   def printable?(list) when is_list(list), do: Enum.all?(list, &(Enum.member?(@ascii_range, &1)))
   def printable?(_), do: false
+
+  def anagram?(word1, word2) when is_binary(word1) and is_binary(word2) do
+    anagram?(String.to_charlist(word1), String.to_charlist(word2))
+  end
+  def anagram?(word1, word2) when is_list(word1) and is_list(word2) do
+    Enum.sort(word1) == Enum.sort(word2)
+  end
 end
