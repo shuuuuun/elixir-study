e519dc22476542cf843db794e121d2506fe188d5
motoki-shun <shuuuuuny+github@gmail.com>
2019-09-22 08:39:51 +0900

    print_table_for_columns

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -34,6 +34,7 @@ defmodule Issues.CLI do
     |> convert_to_list_of_maps
     |> sort_into_ascending_order
     |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}), do: body
